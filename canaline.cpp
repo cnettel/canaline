56ae98380d1b8aca56fa891619d5018c26e1cb51
cnettel <carl.nettelblad@it.uu.se>
2017-11-01 00:34:17 +0100

    A little bit more X3 stuff

--- canaline.cpp
+++ canaline.cpp
@@ -1,9 +1,28 @@
 #include <array>
+#include <boost/spirit/home/x3.hpp>
+
+using namespace boost::spirit;
 
 using box = std::array<int, 4>;
 
-auto prelude = x3::lit('[') ;
-auto box = x3::lit('[') > x3::int_ > ',' x3::int_ > ',' x3::int_ > ',' x3::int_ > ']';
-auto innerboxlist = *(box > ',') > box;
-auto fullboxlist = x3::lit("array") > '(' > '[' > boxlist > ']' > ',' > "dtype" > '=' > "int64" > ')';
+
+auto constexpr bracketize(auto what)
+{
+	return '[' > what > ']';
+}
+
+auto boxrule = bracketize(x3::int_ > ',' > x3::int_ > ',' > x3::int_ > ',' > x3::int_);
+auto innerboxlist = *(boxrule > ',') > boxrule;
+auto fullboxlist = x3::lit("array") > '(' > bracketize(innerboxlist ) > ',' > "dtype" > '=' > "int64" > ')';
+
+auto boxes = bracketize(*(fullboxlist));
+
+auto scorelist = x3::lit("array") > '(' > bracketize(*(x3::double_)) > ')';
+auto scores = bracketize(*(scorelist));
+
+int main()
+{
+	scores.has_action;
+}
+
 
