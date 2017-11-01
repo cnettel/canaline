0ef95036bffa360e94c4fd21f07fb82043ea4eeb
Carl Nettelblad <carl.nettelblad@it.uu.se>
2017-11-01 09:27:08 +0100

    More boilerplate.

--- canaline.cpp
+++ canaline.cpp
@@ -1,7 +1,10 @@
 #include <array>
 #include <boost/spirit/home/x3.hpp>
+#include <iostream>
+#include <fstream>
 
 using namespace boost::spirit;
+using namespace std;
 
 using box = std::array<int, 4>;
 
@@ -20,9 +23,28 @@ auto boxes = bracketize(*(fullboxlist));
 auto scorelist = x3::lit("array") > '(' > bracketize(*(x3::double_)) > ')';
 auto scores = bracketize(*(scorelist));
 
-int main()
+template<class RuleType, class AttrType> void parseToEndWithError(istream& file, const RuleType& rule, AttrType& target)
 {
-	scores.has_action;
+	auto parseriter = boost::spirit::istream_iterator(file);
+	boost::spirit::istream_iterator end;
+
+	bool res = phrase_parse(parseriter, end, rule, x3::space - x3::eol, target);
+
+	if (!res)
+	{
+		std::string val;
+		file >> val;
+		throw logic_error("Parsing failed. " + (std::string) __func__ + " " + val);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 1)
+	{
+		return -1;
+	}
+	ifstream file(argv[1]);
 }
 
 
