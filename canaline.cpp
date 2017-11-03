80a5a0261ca759a1e75cc028973dbf106ed3b186
cnettel <carl.nettelblad@it.uu.se>
2017-11-03 01:59:33 +0100

    Reading lines, rectoring and some hmm logic

--- canaline.cpp
+++ canaline.cpp
@@ -73,7 +73,7 @@ private:
 	map<string, int> mapping;
 	map<int, string> inversemapping;
 public:
-	int getMapping(string word)
+	int getMapping(const string& word)
 	{
 		auto i = mapping.find(word);
 		if (i != mapping.end())
@@ -102,10 +102,59 @@ constexpr std::add_const_t<T>& as_const(const T& t) noexcept
 }
 
 auto word_ = x3::lexeme[+(x3::char_ - x3::space)];
-auto on_word = [&](auto& ctx) {
-	return wordmap.getMapping(x3::_attr(ctx));
-};
-auto linefile = *(*(word_[on_word]) > x3::eol);
+auto linefile = *word_ % x3::eol;
+
+struct hmmtype
+{
+	vector<vector<box>> ourBoxes;
+	vector<vector<double>> scoreVals;
+	vector<bool> lineEnd;
+
+	void prepareLineEnd(const vector<vector<int>>& introws)
+	{
+		lineEnd.reserve(scoreVals.size());
+		for (const auto& r : introws)
+		{
+			for (int i : r)
+			{
+				lineEnd.push_back(false);
+			}
+			*(lineEnd.end() - 1) = true;
+		}
+	}
+
+	void doEmissions(stateVec& state, int pos)
+	{
+		int i = 0;
+		for (auto& val : state)
+		{
+			val *= max(1e-9, scoreVals[pos][i++]);
+		}
+	}
+
+	static double transProb(const box& a, const box& b, bool linebreak)
+	{
+		// TODO
+	}
+
+	void doTransitions(const stateVec& fState, stateVec& toState, int fromPos, int toPos)
+	{
+		for (int i = 0; i < scoreVals[toPos].size(); i++)
+		{
+			toState[i] = 0;
+		}
+
+		for (int i = 0; i < scoreVals[fromPos].size(); i++)
+		{
+			for (int j = 0; j < scoreVals[toPos].size(); j++)
+			{
+
+			}
+		}
+	};
+} hmm;
+
+using stateVec = array<double, 1000>;
 
 int main(int argc, char** argv)
 {
@@ -114,13 +163,40 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	ifstream file(argv[1]);
-	vector<vector<box>> ourBoxes;
-	vector<vector<double>> scoreVals;	
-
-	parseToEndWithError(file, boxes > scores, as_const(std::forward_as_tuple(ourBoxes, scoreVals)));
-	cout << "Read " << ourBoxes.size() << " box lists and " << scoreVals.size() << " score lists." << "\n";
+	
+	parseToEndWithError(file, boxes > scores, as_const(std::forward_as_tuple(hmm.ourBoxes, hmm.scoreVals)));
+	cout << "Read " << hmm.ourBoxes.size() << " box lists and " << hmm.scoreVals.size() << " score lists." << "\n";
 
 	file = ifstream(argv[2]);
-	vector<vector<int>> rows;
+	vector<vector<int>> introws;
+	vector<vector<string>> rows;
+	file >> noskipws;
 	parseToEndWithError(file, linefile, rows);
+
+	transform(rows.begin(), rows.end(), back_inserter(introws),
+		[](vector<string>& row)
+	{
+		vector<int> introw;
+		transform(row.begin(), row.end(), back_inserter(introw),
+			[](const string& word)
+		{
+			return wordmap.getMapping(word);
+		});
+
+		return introw;
+	});
+
+	hmm.prepareLineEnd(introws);	
+
+	stateVec fwbw[2][1000] = { 0 };
+
+	for (int i = 0; i < hmm.scoreVals[0].size(); i++)
+	{
+		fwbw[0][0][i] = 1;
+	}
+
+	for (int i = 0; i < hmm.scoreVals[hmm.scoreVals.size() - 1].size(); i++)
+	{
+		fwbw[1][0][i] = 1;
+	}
 }
