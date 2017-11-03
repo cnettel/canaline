8f85bd52a01aeac1548f36260bb235177fe2122e
Carl Nettelblad <carl.nettelblad@it.uu.se>
2017-11-03 11:01:16 +0100

    Compute fw-bw.

--- canaline.cpp
+++ canaline.cpp
@@ -110,6 +110,8 @@ struct hmmtype
 	vector<vector<double>> scoreVals;
 	vector<bool> lineEnd;
 
+	stateVec fwbw[2][1000] = { 0 };
+
 	void prepareLineEnd(const vector<vector<int>>& introws)
 	{
 		lineEnd.reserve(scoreVals.size());
@@ -123,7 +125,7 @@ struct hmmtype
 		}
 	}
 
-	void doEmissions(stateVec& state, int pos)
+	void emit(stateVec& state, int pos)
 	{
 		int i = 0;
 		for (auto& val : state)
@@ -134,24 +136,98 @@ struct hmmtype
 
 	static double transProb(const box& a, const box& b, bool linebreak)
 	{
-		// TODO
+		bool ok = false;
+		int height = max(a[4] - a[2], b[4] - b[2]);
+		if (linebreak)
+		{
+			if (b[2] > a[2]) ok = true;
+		}
+		else
+		{
+			if (b[1] > a[1] && b[2] > a[2] - height && b[2] < a[2] + height) ok = true;
+		}
+
+		return ok ? 1 : 1e-5;
 	}
 
-	void doTransitions(const stateVec& fState, stateVec& toState, int fromPos, int toPos)
+	template<int dir> void transition(const stateVec& fState, stateVec& toState, int fromPos, int toPos)
 	{
+		static_assert(dir == -1 || dir == 1);
+
 		for (int i = 0; i < scoreVals[toPos].size(); i++)
 		{
-			toState[i] = 0;
+			tState[i] = 0;
 		}
 
 		for (int i = 0; i < scoreVals[fromPos].size(); i++)
 		{
 			for (int j = 0; j < scoreVals[toPos].size(); j++)
 			{
+				box* fromBox = &ourBoxes[fromPos][i];
+				box* toBox = &ourBoxes[toPos][i];
+				int fIndex = fromPos;
 
+				if (dir == -1)
+				{
+					fIndex = toPos;
+					swap(fromBox, toBox);
+				}
+
+				tState[j] += fState[i] * transProb(fromBox, toBox, lineEnd[fIndex]);
 			}
 		}
-	};
+	}
+
+	void normalize(stateVec& state, int pos)
+	{
+		double sum = 0;
+		for (int i = 0; i < scoreVals[pos].size(); i++)
+		{
+			sum += state[i];
+		}
+
+		if (sum < 1e-10)
+		{
+			for (int i = 0; i < scoreVals[pos].size(); i++)
+			{
+				state[i] *= 1e40;
+			}
+		}
+	}
+
+	void computeFB()
+	{
+		// FW
+		for (int i = 0; i < scoreVals[0].size(); i++)
+		{
+			fwbw[0][0][i] = 1;
+		}
+
+		for (int i = 0; i < scoreVals.size(); i++)
+		{
+			emit(fwbw[0][i]);
+			if (i != scoreVals.size() - 1)
+			{
+				transition<1>(fwbw[0][i], fwbw[0][i + 1], i, i + 1);
+			}
+
+			normalize(fwbw[0][i + 1]);
+		}
+
+		// BW
+		for (int i = 0; i < scoreVals[scoreVals.size() - 1].size(); i++)
+		{
+			fwbw[1][0][i] = 1;
+		}
+
+		for (int i = scoreVals.size() - 1; i != 0; i--)
+		{
+			transition<-1>(fwbw[1][i], fwbw[1][i - 1], i, i - 1);
+			emit(fwbw[1][i - 1]);
+
+			normalize(fwbw[1][i - 1]);
+		}
+	}
 } hmm;
 
 using stateVec = array<double, 1000>;
@@ -186,17 +262,6 @@ int main(int argc, char** argv)
 		return introw;
 	});
 
-	hmm.prepareLineEnd(introws);	
-
-	stateVec fwbw[2][1000] = { 0 };
-
-	for (int i = 0; i < hmm.scoreVals[0].size(); i++)
-	{
-		fwbw[0][0][i] = 1;
-	}
-
-	for (int i = 0; i < hmm.scoreVals[hmm.scoreVals.size() - 1].size(); i++)
-	{
-		fwbw[1][0][i] = 1;
-	}
+	hmm.prepareLineEnd(introws);
+	hmm.computeFB();
 }
